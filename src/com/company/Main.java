bfd19c6364f10f00e59c06eda0fdf3c13979df1a
Java02 <alekseenkooleg2004@gmail.com>
2021-02-20 14:23:31 +0200

    harry poter stat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,35 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String paragraph = "THE BOY WHO LIVED \n" +
-                "\n" +
-                "Mr. and Mrs. Dursley, of number four, Privet Drive, \n" +
-                "were proud to say that they were perfectly normal, \n" +
-                "thank you very much. They were the last people you’d \n" +
-                "expect to be involved in anything strange or \n" +
-                "mysterious, because they just didn’t hold with such \n" +
-                "nonsense. \n" +
-                "\n" +
-                "Mr. Dursley was the director of a firm called \n" +
-                "Grunnings, which made drills. He was a big, beefy \n" +
-                "man with hardly any neck, although he did have a \n" +
-                "very large mustache. Mrs. Dursley was thin and \n" +
-                "blonde and had nearly twice the usual amount of \n" +
-                "neck, which came in very useful as she spent so \n" +
-                "much of her time craning over garden fences, spying \n" +
-                "on the neighbors. The Dursley s had a small son \n" +
-                "called Dudley and in their opinion there was no finer \n" +
-                "boy anywhere. ";
-        paragraph = paragraph.replaceAll("\\n", "");
-        paragraph = paragraph.replaceAll("[^A-Za-z ]", "");
-        String[] words = paragraph.split(" ");
-        //System.out.println(words[99] + " " + words[100] + " " + words[101]);
+    public static void main(String[] args) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get("C:\\Users\\Java02\\Downloads\\Harry.txt")));
+        System.out.println(content.length() + " - дожина усього тексту ");
+        content = content.replaceAll("[^A-Za-z ]", "");
+        System.out.println(content.length() + " - без мусора");
+        String[] words = content.split(" +");
+        System.out.println(words.length);
+        System.out.println(words[99] + " " + words[100] + " " + words[101]);
+        int counter = 0;
         for (int i = 0; i < words.length; i++) {
             //System.out.println("" + i + "." + " " + ((char) i ));
-            if (words[i].charAt(0) == 'a' && words[i].length() > 3 && words[i].length() < 10){
+            if (words[i].charAt(0) == 'A'){
                 System.out.println(words[i]);
+                counter++;
             }
+        }
+        System.out.println(counter + " - кількість слів довжиною в 3 букви");
+        System.out.println(words.length);
+        System.out.println(content.substring(0,400));
+        Arrays.sort(words);
+        for (int i = 0; i < 4515; i++) {
+           // System.out.println(words[i]);
         }
     }
 }
